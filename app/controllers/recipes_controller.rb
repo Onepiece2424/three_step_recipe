ed7d494b58f79ad8087950673ccec7d238c27c00
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-02-24 06:37:12 +0900

    recipes controller アクションの作成

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,13 +1,26 @@
 class RecipesController < ApplicationController
-  def index
-  end
+  before_action :authenticate_user!, only: [:top]
 
-  def show
+  def index
   end
 
   def new
   end
 
+  def show
+  end
+
   def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def top
   end
 end
