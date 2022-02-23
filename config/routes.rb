8c9e671cb58d305771ea41b1d71f4f2b52c29361
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-02-23 17:32:32 +0900

    recipesコントローラの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'recipes/index'
+  get 'recipes/show'
+  get 'recipes/new'
+  get 'recipes/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
