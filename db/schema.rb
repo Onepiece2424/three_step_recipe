50eba884d5bc0fbcb8adeb1cf17b2335f3f5d245
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-02-26 19:25:44 +0900

    Recipeモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_23_085510) do
+ActiveRecord::Schema.define(version: 2022_02_26_102318) do
+
+  create_table "recipes", force: :cascade do |t|
+    t.text "process1"
+    t.text "process2"
+    t.text "process3"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
