aac422760fd8ab9f5ffe28a489c7eb5bbcd97c61
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-02-27 08:59:51 +0900

    index,newアクションの変数の設定

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,9 +2,11 @@ class RecipesController < ApplicationController
   before_action :authenticate_user!, except: [:top]
 
   def index
+    @recipes = Recipe.all
   end
 
   def new
+    @recipe = Recipe.new
   end
 
   def show
