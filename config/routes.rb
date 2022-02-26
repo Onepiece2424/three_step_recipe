bd8d25aed03edb79b01d794c449238b479bf10c7
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-02-27 08:59:12 +0900

    recipesのルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   root 'recipes#top'
   devise_for :users
-  get 'recipes/index'
-  get 'recipes/show'
-  get 'recipes/new'
-  get 'recipes/edit'
+  resources :recipes
 end
