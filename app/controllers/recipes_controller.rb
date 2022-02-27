76d6cf3469ad3a5d14200b0cde58da72653857f3
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-02-28 07:14:19 +0900

    recipe contoroller の各アクションの設定

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,20 +9,43 @@ class RecipesController < ApplicationController
     @recipe = Recipe.new
   end
 
+  def create
+    @recipe = Recipe.create(recipe_params)
+    if @recipe.save
+      redirect_to recipes_path
+    else
+      render 'new'
+    end
+  end
+
   def show
+    @recipe = Recipe.find(params[:id])
   end
 
   def edit
+    @recipe = Recipe.find(params[:id])
   end
 
   def update
+    if @recipe.update(recipe_params)
+      redirect_to recipes_path
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    redirect_to recipes_path
   end
 
   private
 
   def top
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:title,:process1,:process2,:process3)
   end
 end
