c3b7e3f5910118890d8af679aee554c76c597792
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-03-05 09:57:13 +0900

    複数枚投稿機能の実装と各ページの画像の表示

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -44,6 +44,6 @@ class RecipesController < ApplicationController
   end
 
   def recipe_params
-    params.require(:recipe).permit(:title,:process1,:process2,:process3)
+    params.require(:recipe).permit(:title,:process1,:process2,:process3, images: [])
   end
 end
