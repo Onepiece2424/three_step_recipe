ab723c568105aaa55672014ecc22bf4064969dbb
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-03-07 06:46:21 +0900

    userが削除されたら、関連するrecipe,commentも削除する

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :recipes
-  has_many :comments
+  has_many :recipes, dependent: :destroy
+  has_many :comments, dependent: :destroy
 end
