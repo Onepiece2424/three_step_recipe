006c50a5b9a25c28acc52e74176348bbd76075c2
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-03-07 07:13:55 +0900

    コメント削除機能の実装（修正途中）

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,6 +9,12 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @comment = Comment.find_by(recipe_id: params[:recipe_id], user_id: current_user.id)
+    @comment.destroy
+    redirect_to recipe_path(params[:recipe_id])
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:comment_content,:recipe_id)
