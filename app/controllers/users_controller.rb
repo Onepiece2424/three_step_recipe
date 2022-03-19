4b501ab2ba767c6e506a41e432b5ba3568efec4b
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-03-19 12:37:49 +0900

    お気に入り登録したレシピの表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @recipes = Recipe.with_attached_images.order(created_at: :desc).limit(5)
+    @favorite_recipes = @user.favorite_recipes
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
   has_many :recipes, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :favorite_recipes, through: :favorites, source: :recipe
 end
