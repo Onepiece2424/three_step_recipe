0ad581791b65ab11a950019b58c143878954a2b2
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-03-19 10:14:55 +0900

    FavoritesControllerのcreate,destroyアクションの実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,13 @@
 class FavoritesController < ApplicationController
   def create
+    favorite = current_user.favorites.build(recipe_id: params[:recipe_id])
+    favorite.save
+    redirect_to recipes_path
   end
 
   def destroy
+    favorite = Favorite.find_by(recipe_id: params[:recipe_id], user_id: current_user.id)
+    favorite.destroy
+    redirect_to recipes_path
   end
 end
