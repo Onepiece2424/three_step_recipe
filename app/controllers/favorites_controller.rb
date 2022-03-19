6806794440bc8e6f877db2fc7a0fcd7094abe0ba
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-03-19 10:54:41 +0900

    お気に入り登録・解除後のフラッシュメッセージの表示

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,12 +2,12 @@ class FavoritesController < ApplicationController
   def create
     favorite = current_user.favorites.build(recipe_id: params[:recipe_id])
     favorite.save
-    redirect_to request.referer
+    redirect_to request.referer, notice: 'お気に入り登録しました！'
   end
 
   def destroy
     favorite = Favorite.find_by(recipe_id: params[:recipe_id], user_id: current_user.id)
     favorite.destroy
-    redirect_to request.referer
+    redirect_to request.referer, notice: 'お気に入り解除しました！'
   end
 end
