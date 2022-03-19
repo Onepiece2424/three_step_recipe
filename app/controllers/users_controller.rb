78686405876becea7bd82b5625d747aa84f8fbd8
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-03-19 12:40:22 +0900

    マイページのCSSの変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @recipes = Recipe.with_attached_images.order(created_at: :desc).limit(5)
+    @recipes = Recipe.with_attached_images.order(created_at: :desc)
     @favorite_recipes = @user.favorite_recipes
   end
 end
