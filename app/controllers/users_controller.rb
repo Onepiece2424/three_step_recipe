7ac9057294d2ecf4dfe360f4f21cb7244909d6d5
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-03-19 12:22:44 +0900

    マイページにレシピの表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @recipes = Recipe.with_attached_images.order(created_at: :desc).limit(5)
   end
 end
