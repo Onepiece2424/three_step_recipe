96a456b01a6d846e53859efeffafd1690cae28c2
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-03-19 15:26:07 +0900

    レシピ編集時にも投稿表示順を最新にするように変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @favorite_recipes = @user.favorite_recipes.order(created_at: :desc)
+    @favorite_recipes = @user.favorite_recipes.order(:updated_at=>:desc,:created_at=>:desc)
   end
 end
