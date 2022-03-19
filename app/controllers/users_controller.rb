90660ecc5a5ad61c6811dd043e2675b4c69af3fa
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-03-19 15:42:43 +0900

    orderメソッドのカラの中身をシンボルで書いた記述に変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @favorite_recipes = @user.favorite_recipes.order(:updated_at=>:desc,:created_at=>:desc)
+    @favorite_recipes = @user.favorite_recipes.order(:updated_at, :created_at).reverse_order
   end
 end
