891a13f7415d0da528b58bab3df2fd482ee1046b
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-03-27 09:46:52 +0900

    プロフィール画像の編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,6 @@ class ApplicationController < ActionController::Base
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username], keys:[:avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:avatar])
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,17 @@
 class UsersController < ApplicationController
-  # def new
-  #   @user = User.new
-  # end
-
-  # def create
-  #   @user = User.create(user_params)
-  #   redirect_to root_path
-  # end
-
   def show
     @user = User.find(params[:id])
     @favorite_recipes = @user.favorite_recipes.order(:updated_at, :created_at).reverse_order
   end
 
-  # private
+  def update
+  @user = User.find(params[:id])
+  @user.update(user_params)
+  redirect_to user_path(@user.id)
+end
 
-  # def user_params
-  #   params.require(:user).permit(:avatar)
-  # end
+  private
+  def user_params
+    params.require(:user).permit(:avatar)
+  end
 end
