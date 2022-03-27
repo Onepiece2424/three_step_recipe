376e01f6cbc2ca377200e1226098da2aec0abf2f
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-03-28 06:45:37 +0900

    インデントの修正と空行の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,13 @@ class UsersController < ApplicationController
   end
 
   def update
-  @user = User.find(params[:id])
-  @user.update(user_params)
-  redirect_to user_path(@user.id)
-end
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
 
   private
+
   def user_params
     params.require(:user).permit(:avatar)
   end
