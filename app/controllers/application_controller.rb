7d6d801c1042bd5926898fc595393793e4ef9576
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-03-28 19:58:37 +0900

    rubocop修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :avatar])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[username avatar])
     devise_parameter_sanitizer.permit(:account_update, keys: [:avatar])
   end
 end
