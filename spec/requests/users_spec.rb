5f2c97d81cff5d6377fdefb191de6c3994e3ab13
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-03-29 07:15:26 +0900

    user ログインテストの作成

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,11 @@
+# require 'rails_helper'
+
+# RSpec.describe User, type: :model do
+#   let(:user) { create(:user) }
+
+#   describe 'ユーザー登録' do
+#     it "username、email、passwordとpassword_confirmationが存在すれば登録できること" do
+#       expect(user).to be_valid
+#     end
+#   end
+# end
