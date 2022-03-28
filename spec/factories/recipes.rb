5897df6029cf8e9e4ba69eada2663ecd847c4d81
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-03-29 06:02:52 +0900

    recipeのテストデータの作成

---
+++ spec/factories/recipes.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :recipe do
+    title              {"test_title"}
+    process1              {"test_process1"}
+    process2              {"test_process2"}
+    process3              {"test_process3"}
+  end
+end
