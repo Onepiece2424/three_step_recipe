398b09487a810def148374ea6217bc12f3d63235
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-03-29 20:00:30 +0900

    インデントの修正

--- spec/factories/recipes.rb
+++ spec/factories/recipes.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :recipe do
-    title              {"test_title"}
+    title                 {"test_title"}
     process1              {"test_process1"}
     process2              {"test_process2"}
     process3              {"test_process3"}
