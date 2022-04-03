531e4cd314be2d9bf5044eecd9a2aea62deac668
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-04-03 09:07:24 +0900

    ログイン画面の表示テストの作成

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -1,10 +1,10 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe "Sessions", type: :request do
-#   describe "GET /sessions/new" do
-#     it 'ログイン画面の表示に成功すること' do
-#       get new_user_session_path
-#       expect(response).to have_http_status(200)
-#     end
-#   end
-# end
+RSpec.describe "Sessions", type: :request do
+  describe "GET /sessions/new" do
+    it 'ログイン画面の表示に成功すること' do
+      get new_user_session_path
+      expect(response).to have_http_status(200)
+    end
+  end
+end
