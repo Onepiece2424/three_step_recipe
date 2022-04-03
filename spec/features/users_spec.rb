a8909a3c8a04f04f01335b713ce3143cf4863d27
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-04-03 11:05:22 +0900

    ユーザー名の表示のテストの作成

---
+++ spec/features/users_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.feature 'ログインとログアウト', type: :feature do
+  given!(:user) { create(:user) }
+  given!(:recipe) { create(:recipe, user: user) }
+
+  scenario 'ログインする' do
+    visit new_user_session_path
+    fill_in "user[email]", with: 'test@gmail.com'
+    fill_in "user[password]", with: '12345678'
+    click_button "Log in（ログイン）"
+    visit recipes_path
+    expect(page).to have_content "Signed in successfully."
+  end
+
+  scenario 'ユーザー名が表示されること' do
+    visit new_user_session_path
+    fill_in "user[email]", with: 'test@gmail.com'
+    fill_in "user[password]", with: '12345678'
+    click_button "Log in（ログイン）"
+    visit recipes_path
+      expect(page).to have_content recipe.user.username
+  end
+end
