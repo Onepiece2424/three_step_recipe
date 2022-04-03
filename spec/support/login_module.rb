9c62bd3b76cc4a4a227f936b4373de840b8f36b8
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-04-03 11:03:49 +0900

    loginモジュールの作成

---
+++ spec/support/login_module.rb
@@ -0,0 +1,8 @@
+module LoginModule
+  def login(user)
+    visit new_user_session_path
+    fill_in 'Email', with: 'test@gmail.com'
+    fill_in 'Password', with: '12345678'
+    click_button 'Log in（ログイン）'
+  end
+end
