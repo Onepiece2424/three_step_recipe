64177d9b48a3582755ef4424b74f49fb8b5c950b
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-04-03 14:05:47 +0900

    レシピ画像表示テストの作成

--- spec/factories/recipes.rb
+++ spec/factories/recipes.rb
@@ -4,5 +4,8 @@ FactoryBot.define do
     process1              {"test_process1"}
     process2              {"test_process2"}
     process3              {"test_process3"}
+    # after(:build) do |recipe|
+    #   recipe.images.attach(io: File.open('spec/fixtures/test.png'), filename: 'test.png', content_type: 'image/png')
+    # end
   end
 end
--- spec/features/recipes_spec.rb
+++ spec/features/recipes_spec.rb
@@ -3,18 +3,16 @@ require 'rails_helper'
 RSpec.feature 'recipeページのテスト' do
   given(:recipe) { create(:recipe, user: user) }
   given(:user) { create(:user) }
-  given(:image) { create(:image) }
 
   background do
     recipe.images = fixture_file_upload("spec/fixtures/test.png")
+    sign_in user
     visit recipes_path
   end
 
   feature 'メインページのテスト' do
     scenario '商品画像が表示されること' do
-      recipe.images.each do |image|
-        expect(page).to have_selector("img,[src$='#{image.filename}']")
-      end
+      expect(page).to have_selector "img,[src$='#{recipe.images}.png']"
     end
   end
 end
