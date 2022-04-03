91896703f55fac7ea9b77aa313c8721bb7a7126a
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-04-03 11:04:53 +0900

    不要な部分の削除

--- spec/requests/recipes_spec.rb
+++ spec/requests/recipes_spec.rb
@@ -3,13 +3,10 @@ require 'rails_helper'
 RSpec.describe "recipeのテスト", type: :request do
   let!(:recipe) { create(:recipe, user: user) }
   let(:user) { create(:user) }
-  # let(:image) { create(:image) }
 
   before do
     sign_in user
     get recipes_path
-    # recipe.images << image
-    recipe = build(:recipe)
     recipe.images = fixture_file_upload("spec/fixtures/test.png")
   end
 
@@ -21,10 +18,10 @@ RSpec.describe "recipeのテスト", type: :request do
     it 'レスポンスにレシピ名が含まれること' do
       expect(response.body). to include recipe.title
     end
-
+   
     it 'レスポンスにレシピ画像が含まれること' do
       recipe.images.each do |image|
-        expect(response.body). to include rails_blob_path(image.first.attachment)
+        expect(response.body).to include rails_blob_path(image.first.attachment)
       end
     end
   end
