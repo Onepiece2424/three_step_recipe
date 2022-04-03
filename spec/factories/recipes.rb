4c19edd786c850b5e20f8d0cdcda0624164a4a22
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-04-03 09:39:38 +0900

    不要な部分の削除

--- spec/factories/recipes.rb
+++ spec/factories/recipes.rb
@@ -4,20 +4,5 @@ FactoryBot.define do
     process1              {"test_process1"}
     process2              {"test_process2"}
     process3              {"test_process3"}
-    user
-    # image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/test.png'))}
-    # after(:build) do |recipe|
-    #   recipe.images.attach(io: File.open('spec/fixtures/test.png'), filename: 'test.png', content_type: 'image/png')
-    # end
-    # trait :image do
-    #   after(:build) do |recipe|
-    #     File.open("#{Rails.root}/spec/fixtures/test.png") do |f|
-    #       recipe.images.attach(io: f, filename: "test.png", content_type: 'image/jpeg')
-    #     end
-    #   end
-    # end
-    # after(:build) do |recipe|
-    #   recipe.images << build(:image, recipe: recipe)
-    # end
   end
 end
