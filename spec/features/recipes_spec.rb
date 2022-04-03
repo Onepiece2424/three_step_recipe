7d31953a765ea73375dc3aa31efe3476ea88d91a
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-04-03 16:32:49 +0900

    Create a new recipeボタンをクリック後、ページ遷移されることのテストの作成

--- spec/features/recipes_spec.rb
+++ spec/features/recipes_spec.rb
@@ -37,5 +37,11 @@ RSpec.feature 'recipeページのテスト' do
     scenario 'have_buttonを用いた新規レシピ作成リンクが表示されること' do
       expect(page).to have_button "Create a new recipe"
     end
+
+    scenario 'Create a new recipeボタンをクリック後、レシピ作成ページへ移動されること' do
+      click_button "Create a new recipe"
+      expect(page).to have_current_path new_recipe_path
+      binding.pry
+    end
   end
 end
