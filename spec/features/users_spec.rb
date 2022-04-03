ccab7ddfe606cb4a344c310506abddb7bc8d8f00
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-04-03 19:07:22 +0900

    マイページのユーザー情報表示テストの作成

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.feature 'ログインとログアウト', type: :feature do
   given(:user) { create(:user) }
   given!(:recipe) { create(:recipe, user: user) }
+  given(:favorite_recipes) { create_list(:recipe, 4, user: user) }
 
   scenario 'ログインする' do
     visit new_user_session_path
@@ -11,5 +12,25 @@ RSpec.feature 'ログインとログアウト', type: :feature do
     click_button "Log in（ログイン）"
     visit recipes_path
     expect(page).to have_content "Signed in successfully."
+  end
+
+  feature 'メインページのテスト' do
+    background do
+      recipe.images = fixture_file_upload("spec/fixtures/test.png")
+      sign_in user
+      visit user_path(user.id)
+    end
+
+    scenario 'ユーザー情報が表示されること' do
+      expect(page).to have_content user.username
+      expect(page).to have_content user.email
+      expect(page).to have_content user.recipes.count
+    end
+
+    scenario 'お気に入りしたレシピが4つ表示されること' do
+      within('.name_follow') do
+        expect(page).to have_selector 'p', count: 4
+      end
+    end
   end
 end
