7db4786f9d545c2da61a74670b9b93f042e12f3d
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-04-17 08:22:12 +0900

    Roleモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_16_051125) do
+ActiveRecord::Schema.define(version: 2022_04_16_231744) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -66,6 +66,12 @@ ActiveRecord::Schema.define(version: 2022_04_16_051125) do
     t.string "title"
     t.integer "user_id"
     t.index ["user_id"], name: "index_recipes_on_user_id"
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string "authority_name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
