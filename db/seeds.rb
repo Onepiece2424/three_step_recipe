c4ca49dda493f8e2b08cbede76e20f52c20f580c
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-04-17 21:18:55 +0900

    Userにadminを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.all.each do |user|
-  user.recipes.create!(
-    title: 'チャーハン',
-    process1: '具材を切る',
-    process2: '炒める',
-    process3: '味付け'
-  )
-end
+# User.all.each do |user|
+#   user.recipes.create!(
+#     title: 'チャーハン',
+#     process1: '具材を切る',
+#     process2: '炒める',
+#     process3: '味付け'
+#   )
+# end
+
+User.create!(username:  "管理者",
+  email: "admin@example.jp",
+  password:  "11111111",
+  password_confirmation: "11111111",
+  admin: true)
