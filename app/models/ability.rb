f35d7006b91269fb7e90b262053929e17f6c14bd
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-04-18 07:02:16 +0900

    abilityの機能の追加

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,17 +4,17 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # can :manage, :session
-    # user ||= User.new
-    # if user.admin?
-    #   can :manage, :all
-    #   can :access_admin_page, :all
-    # elsif user.general?
-    #   can :manage, :all
-    #   cannot :access_admin_page, :all
-    # else
-    #   can :read, :all
-    #   cannot :access_admin_page, :all
-    # end
+    can :manage, :session
+    user ||= User.new
+    if user.admin?
+      can :manage, :all
+      can :access_admin_page, :all
+    elsif user.general?
+      can :manage, :all
+      cannot :access_admin_page, :all
+    else
+      can :read, :all
+      cannot :access_admin_page, :all
+    end
   end
 end
