fffc7f01a31484c66f125f4e7d4a18c5305d5f50
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-04-20 20:38:17 +0900

    管理者ページのリンクの表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root 'recipes#top'
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
+  namespace :admin do
+    resources :recipes do
+      collection do
+        get 'search'
+      end
+      resources :comments, only:[:create, :destroy]
+      resource :favorites, only: [:create, :destroy]
+    end
+    resources :users
+  end
   resources :recipes do
     collection do
       get 'search'
