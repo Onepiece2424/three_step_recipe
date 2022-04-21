7efe9de747287bfa6aec35f3237a9d4a610ce994
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-04-21 21:43:24 +0900

    不要なmigrationファイルの削除

--- db/migrate/20220418205534_removecolumn_to_role.rb
+++
@@ -1,8 +0,0 @@
-class RemovecolumnToRole < ActiveRecord::Migration[6.1]
-  def change
-    def up
-      remove_column :authority_name, :string
-      remove_column :created_at, :updated_at, :datetime
-    end
-  end
-end
