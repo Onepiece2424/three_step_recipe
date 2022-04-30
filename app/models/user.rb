50908f1b0ca9448364b69cb33a1d07761bd23247
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-04-30 15:52:17 +0900

    guest userのデフォルトのroleカラムの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
   has_one_attached :avatar
 
   def self.guest
-    find_or_create_by!(email: 'guest@example.com') do |user|
+    find_or_create_by!(email: 'guest@example.com',role: 'guest') do |user|
       user.password = SecureRandom.urlsafe_base64
     end
   end
