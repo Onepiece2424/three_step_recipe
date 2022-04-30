62588777c8da73516ab3a8d0de1868f6493c8254
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-04-30 17:01:27 +0900

    exceptionの書き換え

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   authorize_resource class: false
 
-  rescue_from CanCan::AccessDenied do |exception|
+  rescue_from CanCan::AccessDenied do |_exception|
     redirect_to recipes_path
   end
 
