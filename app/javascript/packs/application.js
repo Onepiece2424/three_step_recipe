802bcbd277b76a39ff1b8aa1750633730db5291f
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-06-02 05:36:25 +0900

    gem 'react-rails'の導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,3 +13,7 @@ Turbolinks.start()
 ActiveStorage.start()
 require('jquery')
 require("test.js")
+// Support component names relative to this directory:
+var componentRequireContext = require.context("components", true);
+var ReactRailsUJS = require("react_ujs");
+ReactRailsUJS.useContext(componentRequireContext);
